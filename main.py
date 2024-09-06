d4c2bd276a0a82d0689ab07902a67709e3990203
Marliece <180341631+Marliece@users.noreply.github.com>
2024-09-06 16:15:15 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+print("Marvel Movie Character Creator")
+print("--")
+hang = input("Do you like 'hanging around'?: ")
+if  hang == "yes":
+  print("You are Spider-man")
+else:
+  print("Then you're not Spider-man")
+print()
+voice = input("Do you have a 'gravelly' voice?: ")
+if voice == "yes":
+  print("You are Korg")
+else:
+  print("Aww, then you're not Korg")
+print()
+marvel = input("Do you often feel 'Marvelous'?: ")
+if marvel == "yes":
+  print("Aha! You're Captain Marvel! Hi!")
+else:
+  print("You are not Captain Marvel")
